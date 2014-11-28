36e6a0ca435f169dce507a2d9eee5b90bc090e95
moroz <moroz@gmail.com>
2014-11-28 19:55:44 +0200

    lets do it

--- BugTrecker/index.php
+++ BugTrecker/index.php
@@ -5,24 +5,16 @@
  * Date: 13.11.14
  * Time: 19:45
  */
-require 'Class/Task.php';
-ini_set('display_errors','On');
-error_reporting(E_ALL);
-$pdo = new \PDO('mysql:dbname=BugTrecker;host=127.0.0.1','root','67890as',
-    [PDO::ATTR_PERSISTENT =>true]);
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    if(isset($_POST['taskname']) & (isset($_POST['description']))){
-        $taskn = $_POST['taskname'];
-        $descrn = $_POST['description'];
-        $task = new Task();
-        $task->addNewTask($taskn,$descrn,$pdo);
-        header('Location:index.html');
-    }
-    if(isset($_POST['ShowTask']) & $_POST['ShowTask']==true){
-        $task = new Task();
-        $task->GetAllTasks($pdo);
-    }
-}
+spl_autoload_register(function ($class){
+    include str_replace('\\','/',$class). '.php';
+});
+use base\App;
+
+$app = new App();
+$app->run();
+//require 'Created/Task.php';
+
+
 //$task = new Task();
 //$task->GetAllTasks($pdo);
 
